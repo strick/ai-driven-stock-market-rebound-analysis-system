dbfd4bf905820cba58bfff61b211b53e776c9ce1
Brian Strickland <bstrick@gmail.com>
2024-01-29 18:37:36 -0500

    Skeleton and API verfication app AB#632

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import requests
+import os
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+load_dotenv()  # This is the important part
+
+@app.route('/')
+
+def get_stock_data(stock_symbol='IFRX'):
+    api_key = os.getenv('ALPHA_VANTAGE_API_KEY')  # Accessing the API key
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={api_key}'
+    r = requests.get(url)
+    data = r.json()
+    return data  # Add this line to return the data
+
+
+import json  # Add this import
+
+def home():
+    data = get_stock_data('IFRX')
+    return json.dumps(data)  # Convert dict to JSON string
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
