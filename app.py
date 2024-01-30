e48e1d28d59814053f6e50f120231c179f91c58a
Brian Strickland <bstrick@gmail.com>
2024-01-29 22:23:29 -0500

    Refactor, fixed get stock data issue  AB#629

--- app.py
+++ app.py
@@ -2,26 +2,23 @@ from flask import Flask
 import requests
 import os
 from dotenv import load_dotenv
-import json  # Add this import
+import json
 
 app = Flask(__name__)
-load_dotenv()  # This is the important part
+load_dotenv()
 
-@app.route('/')
-
-def get_stock_data(stock_symbol='IFRX'):
-    api_key = os.getenv('ALPHA_VANTAGE_API_KEY')  # Accessing the API key
+# This is just a regular function, not a route
+def get_stock_data(stock_symbol):
+    api_key = os.getenv('ALPHA_VANTAGE_API_KEY')
     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock_symbol}&apikey={api_key}'
     r = requests.get(url)
     data = r.json()
-    return data  # Add this line to return the data
+    return data
 
+@app.route('/')
 def home():
-    data = get_stock_data('IFRX')
-    return json.dumps(data)  # Convert dict to JSON string
-
+    data = get_stock_data('IFRX')  # Call get_stock_data with the desired symbol
+    return json.dumps(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # Test
-
